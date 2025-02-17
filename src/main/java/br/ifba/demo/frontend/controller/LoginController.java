e88368e877ee3a391ab807431e6e188109bf6e4b
thiagoGNB2003 <thiagogiovanni19@gmail.com>
2025-02-17 19:06:30 -0300

    com1

--- src/main/java/br/ifba/demo/frontend/controller/LoginController.java
+++ src/main/java/br/ifba/demo/frontend/controller/LoginController.java
@@ -19,6 +19,16 @@ public class LoginController {
 	public String home() {
 		return "home";
 	}
+
+	@GetMapping("/instrutor")
+	public String instrutor() {
+		return "instrutor";
+	}
+
+	@GetMapping("/lista")
+	public String lista() {
+		return "lista";
+	}
 	
 	@GetMapping("/login")
 	public String login() {
